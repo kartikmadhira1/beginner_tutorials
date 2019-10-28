a2d031e59f1ac1093f7ea82aa7dd17131a68bd3f
Kartik Madhira <kartikmadhira1@gmail.com>
2019-10-28 05:27:38 -0400

    fixed lint and cppcheck errors, added outputs to results folder.

--- src/listener.cpp
+++ src/listener.cpp
@@ -38,13 +38,11 @@
  * @brief  Callback function to subscribe on the chatter topic
  * @param  msg   The request data sent to service
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
