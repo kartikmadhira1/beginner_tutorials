473f20cfe47b7f0b810acbc80920dcf04a483951
Kartik Madhira <kartikmadhira1@gmail.com>
2019-10-28 05:11:10 -0400

    added c++11 features and doxygen style commenting

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,8 +1,42 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Kartik Madhira
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to
+ *  whom the Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *
+ *  @file    listener.cpp
+ *  @author  Kartik Madhira
+ *  @copyright MIT License
+ *
+ *  @brief  ENPM808X :Assignment to implement ROS publisher and subscriber nodes
+ *
+ */
+
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
 /**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
+ * @brief  Callback function to subscribe on the chatter topic
+ * @param  msg   The request data sent to service
  */
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
@@ -45,7 +79,7 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  auto sub = n.subscribe("chatter", 1000, chatterCallback);
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
