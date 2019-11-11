2b0212a60e85fe0687f73c815b1876ac9b7ff077
Kartik Madhira <kartikmadhira1@gmail.com>
2019-11-10 21:51:44 -0500

    added talker based node tests

--- test/testTalker.cpp
+++ test/testTalker.cpp
@@ -1,9 +1,26 @@
-#include "ros/ros.h"
-#include <gtest/gtest.h>
+// Copyright 2019, Kartik Madhira
 
-TEST(TestSuite, testCase1) {
-    bool flag = true;
-    EXPECT_TRUE(flag);
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+#include <ros/service_client.h>
+#include <tf/transform_listener.h>
+#include "beginner_tutorials/modify_string.h"
+
+TEST(TESTSuite, testService) {   
+    ros::NodeHandle handler;
+    ros::ServiceClient srvClient = handler.serviceClient<
+                                beginner_tutorials::modify_string>
+                                ("modify_string");   
+    bool srvBool(srvClient.waitForExistence(ros::Duration(3)));   
+    EXPECT_TRUE(srvBool);   
+}
+
+TEST(TESTSuite, testTransform) {   
+    tf::TransformListener listener;
+    tf::Transform tf;
+    bool listenBool(listener.waitForTransform("world", "talk", 
+                                            ros::Time(), ros::Duration(1.0)));
+    EXPECT_TRUE(listenBool);   
 }
 
 // Run all the tests that were declared with TEST()
